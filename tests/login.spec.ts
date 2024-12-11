587c9175988dd42ebe8d48c14cf96f0c3bd95f3c
Gary Behan <gary.b@browserstack.com>
2024-12-11 13:33:24 +0000

    Initial Commit

---
+++ tests/login.spec.ts
@@ -0,0 +1,63 @@
+// @ts-check
+export {};
+const { test, expect } = require('@playwright/test');
+
+var username = "student";
+var wrongUsername = "wrongStudent";
+var password = "Password123";
+var wrongPassword = "Password321";
+var wrongUsernameText = "Incorrect username!";
+var wrongPasswordText = "Incorrect password!";
+var wrongUsernameAndPasswordText = "Incorrect username and password!";
+
+test('A successful login was performed', async ({ page } : { page:any }) => {
+  await page.goto('http://browserstacktraining.site');
+
+  await page.locator("#loginButton").click();
+
+  await page.locator("#username").fill(username);
+  await page.locator("#password").fill(password);
+  await page.locator("#submit").click();
+
+  // Expect a title "to contain" a substring.
+  await expect(page.locator("#message")).toHaveText("Login successful!");
+});
+
+test('The wrong username was entered', async ({ page } : { page:any }) => {
+  await page.goto('http://browserstacktraining.site');
+
+  await page.locator("#loginButton").click();
+
+  await page.locator("#username").fill(wrongUsername);
+  await page.locator("#password").fill(password);
+  await page.locator("#submit").click();
+
+  // Expect a title "to contain" a substring.
+  await expect(page.locator("#message")).toHaveText(wrongUsernameText);
+});
+
+test('The wrong password was entered', async ({ page } : { page:any }) => {
+  await page.goto('http://browserstacktraining.site');
+
+  await page.locator("#loginButton").click();
+
+  await page.locator("#username").fill(username);
+  await page.locator("#password").fill(wrongPassword);
+  await page.locator("#submit").click();
+
+  // Expect a title "to contain" a substring.
+  await expect(page.locator("#message")).toHaveText(wrongPasswordText);
+});
+
+test('Both the wrong username and password were entered', async ({ page } : { page:any }) => {
+  await page.goto('http://browserstacktraining.site');
+
+  await page.locator("#loginButton").click();
+
+  await page.locator("#username").fill(wrongUsername);
+  await page.locator("#password").fill(wrongPassword);
+  await page.locator("#submit").click();
+
+  // Expect a title "to contain" a substring.
+  await expect(page.locator("#message")).toHaveText(wrongUsernameAndPasswordText);
+});
