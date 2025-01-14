528bf775d9f2357e4668ce5725f060812c6c523c
Gary Behan <gary.b@browserstack.com>
2025-01-14 15:26:42 +0000

    Initial commit

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,7 +1,6 @@
 // @ts-check
 export {};
 const { test, expect } = require('@playwright/test');
-const percySnapshot = require('@percy/playwright');
 
 var username = "student";
 var wrongUsername = "wrongStudent";
@@ -16,16 +15,12 @@ test('A successful login was performed', async ({ page } : { page:any }) => {
 
   await page.locator("#loginButton").click();
 
-  await percySnapshot(page, 'Login Page');
-
   await page.locator("#username").fill(username);
   await page.locator("#password").fill(password);
   await page.locator("#submit").click();
 
   // Expect a title "to contain" a substring.
   await expect(page.locator("#message")).toHaveText("Login successful!");
-
-  await percySnapshot(page, 'Login Successful');
 });
 
 test('The wrong username was entered', async ({ page } : { page:any }) => {
