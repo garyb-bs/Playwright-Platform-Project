4aeac3b4e691e1c12e8b64337a41bca6021ba15f
Gary Behan <gary.b@browserstack.com>
2025-03-19 11:04:45 +0000

    Updates

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,7 @@
 // @ts-check
 export {};
 const { test, expect } = require('@playwright/test');
+const percySnapshot = require('@percy/playwright');
 
 var username = "student";
 var wrongUsername = "wrongStudent";
@@ -11,9 +12,9 @@ var wrongPasswordText = "Incorrect password!";
 var wrongUsernameAndPasswordText = "Incorrect username and password!";
 
 test('A successful login was performed', async ({ page } : { page:any }) => {
-  await page.goto('http://browserstacktraining.site');
+  await page.goto('https://practicetestautomation.com/practice-test-login/');
 
-  await page.locator("#loginButton").click();
+  await percySnapshot(page, 'Login Page');
 
   await page.locator("#username").fill(username);
   await page.locator("#password").fill(password);
@@ -21,12 +22,12 @@ test('A successful login was performed', async ({ page } : { page:any }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page.locator("#message")).toHaveText("Login successful!");
+
+  await percySnapshot(page, 'Login Successful');
 });
 
 test('The wrong username was entered', async ({ page } : { page:any }) => {
-  await page.goto('http://browserstacktraining.site');
-
-  await page.locator("#loginButton").click();
+  await page.goto('https://practicetestautomation.com/practice-test-login/');
 
   await page.locator("#username").fill(wrongUsername);
   await page.locator("#password").fill(password);
@@ -37,9 +38,7 @@ test('The wrong username was entered', async ({ page } : { page:any }) => {
 });
 
 test('The wrong password was entered', async ({ page } : { page:any }) => {
-  await page.goto('http://browserstacktraining.site');
-
-  await page.locator("#loginButton").click();
+  await page.goto('https://practicetestautomation.com/practice-test-login/');
 
   await page.locator("#username").fill(username);
   await page.locator("#password").fill(wrongPassword);
@@ -50,9 +49,7 @@ test('The wrong password was entered', async ({ page } : { page:any }) => {
 });
 
 test('Both the wrong username and password were entered', async ({ page } : { page:any }) => {
-  await page.goto('http://browserstacktraining.site');
-
-  await page.locator("#loginButton").click();
+  await page.goto('https://practicetestautomation.com/practice-test-login/');
 
   await page.locator("#username").fill(wrongUsername);
   await page.locator("#password").fill(wrongPassword);
